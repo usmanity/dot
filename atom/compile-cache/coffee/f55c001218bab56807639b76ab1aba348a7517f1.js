d067442be2e92ab3442d46f2136374577db662ae
Muhammad Usman <muhammad@weebly.com>
2016-02-12 11:42:53 -0800

    removing subl and atom settings

--- atom/compile-cache/coffee/f55c001218bab56807639b76ab1aba348a7517f1.js
+++
@@ -1,8 +0,0 @@
-(function() {
-
-
-}).call(this);
-
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiL1VzZXJzL211aGFtbWFkLy5hdG9tL2luaXQuY29mZmVlIgogIF0sCiAgIm5hbWVzIjogW10sCiAgIm1hcHBpbmdzIjogIkFBaUp5QztBQUFBO0FBQUEiCn0=
-
-//# sourceURL=/Users/muhammad/.atom/init.coffee
