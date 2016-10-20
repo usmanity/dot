b06b446f54589a4dbe9257bfb52b6c944ede7d6d
Muhammad Usman <muhammad@weebly.com>
2016-10-19 23:31:56 -0700

    only current git branch to push

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "git will now use simple strategy for pulling and pushing"
-git config --global push.default simple
+
+git config --global push.default current
 
 echo "Setting up symlinks..."
+
 if [ -f ~/.vimrc ]
   then
     echo "~/.vimrc exists already, skipping"
